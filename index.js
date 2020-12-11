291e2febc553a2d64a707581ef7f97eb761dc981
embarekmessai <m_mebarek@hotmail.com>
2020-12-12 00:14:38 +0100

    Add nodejs api whith routes

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+
+const docRoutes = require('./api/routes/docs')
+
+/** Routes to handle requests */
+app.use('/docs', docRoutes);
+
+/** Handle Errors */
+app.use((req, res, next)=>{
+    const error = new Error('Not found')
+    error.status = 404
+    next(error)
+})
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500)
+    res.json({
+        error:{
+            message: error.message
+        }
+    })
+})
+
+module.exports = app;
